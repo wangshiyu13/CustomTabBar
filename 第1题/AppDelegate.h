2bc2c29a127d36fea8615876ab776ef3efdfe13d
wangshiyu13 <wangshiyu13z@gmail.com>
2015-01-05 09:25:01 +0800

    Initial Commit

---
+++ 第1题/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  第1题
+//
+//  Created by wangshiyu13 on 15/1/5.
+//  Copyright (c) 2015年 wangshiyu13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 第1题/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  第1题
+//
+//  Created by wangshiyu13 on 15/1/5.
+//  Copyright (c) 2015年 wangshiyu13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
